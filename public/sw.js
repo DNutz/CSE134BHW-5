a6624d03dd6da9440cbb5545548c9d856b4dfb98
Dnutz <nuttall_5@hotmail.com>
2017-12-08 22:22:51 -0800

    a few more last minute adds

--- public/sw.js
+++ public/sw.js
@@ -1,9 +1,6 @@
-// Cache ID Version
 const cacheID = 'v1';
-// Files to precache
 const cacheFiles = [
   '/',
-  // HTML Files
   './index.html',
   './homepage.html',
   './create_account.html',
@@ -16,12 +13,10 @@ const cacheFiles = [
   './player_info_vanilla.html',
   './input_stats_vanilla.html',
   './gamestat_summary.html',
-  // CSS Files
-  // Image Files
+  './offline.html',
   './images/edit.svg',
   './images/LAGalaxy.svg',
   './images/soccer_icon.png',
-  // JS Files
   './js/schedule.js',
   './js/hideInputTab.js',
   './js/CRUD_roster.js',
@@ -30,32 +25,25 @@ const cacheFiles = [
   './js/firestore.js',
   './sw.js',
   './js/app.js',
-  // Misc. Files
   './manifest.json',
 ];
 
-// Service Worker Install Event
 self.addEventListener('install', function(event) {
-  console.log('Attempting to install service worker and cache static assets');
   event.waitUntil(
     caches.open(cacheID)
     .then(function(cache) {
       return cache.addAll(cacheFiles);
     })
     .catch(function(error) {
-      console.log(`Unable to add cached assets: ${error}`);
     })
   );
 });
 
-// Service Worker Activate Event
 self.addEventListener('activate', function(e) {
   e.waitUntil(
-    // Load up all items from cache, and check if cache items are not outdated
     caches.keys().then(function(keyList) {
       return Promise.all(keyList.map(function(key) {
         if (key !== cacheID) {
-          console.log('[ServiceWorker] Removing old cache', key);
           return caches.delete(key);
         }
       }));
@@ -68,45 +56,27 @@ self.addEventListener('fetch', function(event) {
     event.respondWith(
       caches.match(event.request)
         .then(function(response) {
-          console.log('event');
-          // Cache hit - return response
           if (response) {
-            console.log('hit');
             return response;
           }
-  
-          // IMPORTANT: Clone the request. A request is a stream and
-          // can only be consumed once. Since we are consuming this
-          // once by cache and once by the browser for fetch, we need
-          // to clone the response.
           var fetchRequest = event.request.clone();
-          console.log(fetchRequest.method);
   
           return fetch(fetchRequest).then(
             function(response) {
-              console.log('here');
-              console.log(response.type);
-              // Check if we received a valid response
               if(!response || response.status !== 200 || fetchRequest.method == 'POST') {
-                console.log('invalid');
                 return response;
               }
-  
-              // IMPORTANT: Clone the response. A response is a stream
-              // and because we want the browser to consume the response
-              // as well as the cache consuming the response, we need
-              // to clone it so we have two streams.
-              console.log('there');
               var responseToCache = response.clone();
               caches.open(cacheID)
                 .then(function(cache) {
-                  console.log('cached');
                   cache.put(event.request, responseToCache);
                 });
   
               return response;
             }
           );
+        }).catch(function() {
+          return caches.match('./offline.html');
         })
       );
   });
